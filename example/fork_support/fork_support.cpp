ead75383afe00bcc472a13de16f1e8d99b606e07
Martin Plattner <mrtn@plattner.me>
2019-08-25 17:56:28 +0200

    test: add current version of fork_support target

--- example/fork_support/fork_support.cpp
+++ example/fork_support/fork_support.cpp
@@ -1,19 +1,70 @@
 #include <iostream>
 #include <blocksci/blocksci.hpp>
+#include <blocksci/core/chain_manager.hpp>
 
 int main(int argc, const char * argv[]) {
-    std::cout << "### Loading BTC ###" << std::endl;
-    //blocksci::Blockchain btc{"/mnt/data/blocksci/bitcoin/current-v0.6/config.json"};
-    blocksci::Blockchain btc{"/home/martin/testchains/blocksci/btc-btc/btc-fork-config.json"};
 
-    std::cout << "### Loading BCH ###" << std::endl;
+    blocksci::ChainManager asd;
+    asd.loadFullChain("/home/martin/testchains/blocksci/btc-btc/btc-fork-config.json");
+
+    return 0;
+
+    std::cout << "### Loading BTC fork ###" << std::endl;
+    //blocksci::Blockchain btc{"/mnt/data/blocksci/bitcoin/current-v0.6/config.json"};
+    blocksci::Blockchain btcFork{"/home/martin/testchains/blocksci/btc-btc/btc-fork-config.json"};
+
+    std::cout << "### Loading BTC main ###" << std::endl;
+    blocksci::Blockchain btcMain{"/home/martin/testchains/blocksci/btc-btc/btc-main-config.json"};
+
     //blocksci::Blockchain bch{"/mnt/data/blocksci/bitcoin-cash/current/config_fork_support.json"};
 
-
-    RANGES_FOR(auto block, btc) {
-        if (block.size() > 1) {
-            std::cout << "### Block " << block.height() << ": " << block.size() << " transactions" << std::endl;
+    /*
+    for(auto block: btcFork) {
+        for(auto tx: block) {
+            std::cout << "tx: " << tx.txNum << "(size " << tx.totalSize() << ")" << std::endl;
         }
+    }
+    return 0;
+
+    for (int i=0; i < 15; i++) {
+        auto btcForkBlock = btcFork[i];
+        auto btcMainBlock = btcMain[i];
+
+        std::cout << "### Block " << i << " ### " << std::endl;
+        std::cout << "btcMain.hash = " << btcMainBlock.getHeaderHash() << std::endl;
+        std::cout << "btcFork.hash = " << btcForkBlock.getHeaderHash() << std::endl;
+
+
+
+
+        for(int j=0; j < btcForkBlock.size(); j++) {
+            auto btcForkTx = btcForkBlock[j];
+            auto btcMainTx = btcMainBlock[j];
+
+            std::cout << "tx: " << btcForkTx.txNum << std::endl;
+
+            auto btcForkOutputRange = btcForkTx.outputs();
+            auto btcMainOutputRange = btcMainTx.outputs();
+
+            for (int k=0; k < btcForkTx.outputCount(); k++) {
+                auto btcForkOutput = btcForkOutputRange[k];
+                auto btcMainOutput = btcMainOutputRange[k];
+
+                std::cout << "output " << btcForkOutput.pointer.inoutNum << ": spendingTx = " << btcMainOutput.getSpendingTxIndex().value_or(0)
+                    << " | " << btcForkOutput.getSpendingTxIndex().value_or(0) << std::endl;
+            }
+
+            //std::cout << "btcForkTx.spendingTx = " << btcForkTx
+        }
+    }
+
+    return 0;
+    */
+
+    RANGES_FOR(auto block, btcMain) {
+        //if (block.size() > 1) {
+            std::cout << "### Block " << block.height() << ": " << block.size() << " transactions" << std::endl;
+        //}
         if (block.height() > 5000) {
             break;
         }
@@ -22,12 +73,7 @@ int main(int argc, const char * argv[]) {
             std::cout << "tx: " << tx.txNum << "(size " << tx.totalSize() << ")" << std::endl;
 
             RANGES_FOR(auto output, tx.outputs()) {
-                std::cout << "output: " << output.getValue() << std::endl;
-            }
-            if (tx.txNum == 727) {
-                // break out of loops as the asset(index < size()) fails in the file mapper, because of the
-                // resetTxData() in TransactionRange::iterator::self_type &operator++() {
-                goto afterLoop;
+                std::cout << "output: " << output.getValue() << " | spendingTx: " << output.getSpendingTxIndex().value_or(0) << std::endl;
             }
         }
     }
@@ -67,7 +113,7 @@ int main(int argc, const char * argv[]) {
     }
     */
 
-    std::getchar();
+    //std::getchar();
 
     return 0;
 }
