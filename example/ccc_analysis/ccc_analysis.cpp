b7cecbf21eee5e8b7a193412f48e34c0c3dc03d7
Martin Plattner <mrtn@plattner.me>
2019-12-05 00:47:52 +0100

    feat: fix sameAsOnForkHeight property in ccc analysis

--- example/ccc_analysis/ccc_analysis.cpp
+++ example/ccc_analysis/ccc_analysis.cpp
@@ -131,7 +131,7 @@ int main(int argc, char * argv[]) {
 
         if (bchAddressCount == 0) {
             btcOnlyCluster = true;
-            //sameAsOnForkHeight = false;
+            sameAsOnForkHeight = false;
             btcOnlyClusterCount++;
         }
         else {
